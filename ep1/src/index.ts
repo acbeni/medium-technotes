4ba3670d70905351ea855a56fc5a164445ffa354
abeniasaad <achraf.beniasaad@commercetools.de>
2019-11-27 21:33:20 +0100

    fix data

--- ep1/src/index.ts
+++ ep1/src/index.ts
@@ -1,29 +1,4 @@
 import { generateSchemaToPath } from "./helper";
 import { func } from "joi";
 
-generateSchemaToPath("/Users/abeniasaad/IdeaProjects/newTsSdk/ctp.graphql")
-
-
-// const query = gql`
-//   query {
-//     carts {
-//       count
-//       results {
-//         country
-//         taxMode
-//         billingAddress {
-//           country
-//           city,
-//           building,
-//         }
-//         createdBy{
-//             clientId
-//         }
-//       }
-//     }
-//   }
-// `;
-
-// function gql(val:TemplateStringsArray){
-//     console.log(JSON.stringify(val))
-// }
+generateSchemaToPath("ctp.graphql")
